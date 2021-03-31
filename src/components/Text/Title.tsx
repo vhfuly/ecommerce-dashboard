356903ed145227f9fc942397897c45797a9f7715
Victor Fuly <victor_hugo_fuly@hotmail.com>
2021-03-30 21:17:52 -0300

    feat: create PurchaseDetails

--- src/components/Text/Title.tsx
+++ src/components/Text/Title.tsx
@@ -9,6 +9,8 @@ function Title(props: typesProps) {
   switch(props.type) {
     case 'h1':
       return (<h1 className='title'>{props.title}</h1>);
+    case 'h2':
+    return (<h2 className='title'>{props.title}</h2>);
     default:
       return (<h1 className='title'>{props.title}</h1>);
   }
--- src/containers/Purchase/PurchaseDetails.tsx
+++ src/containers/Purchase/PurchaseDetails.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import Title from '../../components/Text/Title';
+import ButtonSimple from '../../components/Buttons/Simple';
 
 const PurchaseDetails: React.FC = () => {
 
   return(
     <div className='container-purchase-details'>
-    PurchaseDetails
+      <div>
+        <Title type='h2' title='pedido - cliente 1 - 04/03/2021'/>
+        <ButtonSimple 
+          type='danger'
+          label='Cancelar Pedido'
+          onClick={() => alert('Cancelado...!')}
+        />
+      </div>
+
     </div>
   );
 }
