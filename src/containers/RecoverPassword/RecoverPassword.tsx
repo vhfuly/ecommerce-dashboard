775ef11c757ae9b655c7a952035ca2af6eb661ee
Victor Fuly <victor_hugo_fuly@hotmail.com>
2021-04-07 18:10:51 -0300

    feat: add styles on recover-password

--- src/containers/RecoverPassword/RecoverPassword.tsx
+++ src/containers/RecoverPassword/RecoverPassword.tsx
@@ -3,7 +3,7 @@ import Title from '../../components/Text/Title';
 import Input from '../../components/Inputs/Simple';
 import Button from '../../components/Buttons/Simple';
 
-
+import '../../styles/containers/recover-password.css'
 const RecoverPassword: React.FC = () => {
   const [email, setEmail ] = useState<string>('');
   const onchangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -11,26 +11,28 @@ const RecoverPassword: React.FC = () => {
   }
   return(
     <div className='container-recover-password'>
-      <div>
-        <Title type='h1' title='LOJA'/>
+      <div className='card'>
+        <div>
+          <Title type='h1' title='LOJA'/>
 
-        <p>
-          Para reiniciar sua senha , digite seu email abaixo.
-        </p>
-        <p>
-          Iremos enviar um link para você acessar e entrar uma nova senha.
-        </p>
-      </div>
-      <div>
-      <Input
-        label="E-mail"
-        value={email}
-        onChange={(event: React.ChangeEvent<HTMLInputElement>) => onchangeInput(event)}
-        type="email" />
-      </div>
-      <br />
-      <div className="flex flex-center">
-        <Button type="success" route='/reset-password/1' label="RESETAR SENHA" />
+          <p>
+            Para reiniciar sua senha , digite seu email abaixo.
+          </p>
+          <p>
+            Iremos enviar um link para você acessar e entrar uma nova senha.
+          </p>
+        </div>
+        <div>
+        <Input
+          label="E-mail"
+          value={email}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => onchangeInput(event)}
+          type="email" />
+        </div>
+        <br />
+        <div className="button-reset">
+          <Button type="success" route='/reset-password/1' label="RESETAR SENHA" />
+        </div>
       </div>
     </div>
   );
